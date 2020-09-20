69dbbe03a2e6ef68525d99c462c6b2a23086f61c
Zubayr-dev <68279853+Zubayr-dev@users.noreply.github.com>
2020-09-20 13:56:55 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+function toglemenu(){
+    const toglemenu=document.querySelector('.toglemenu');
+    const navigation=document.querySelector('.navigation');
+    toglemenu.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
